43be397f6de79b7cd31c07a0fab199c3fc1f5e44
Ivan Garcia <ivanvlora@gmail.com>
2018-12-02 12:51:20 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+const shout = (string) => {
+  return string.toUpperCase();
+}
+
+const whisper = (string) => {
+  return string.toLowerCase();
+}
+
+const logShout = string => {
+  return console.log(string.toUpperCase());
+}
